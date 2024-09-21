3b8304c3cabcf6df83ded76e0dbd71998247b6fb
Ishan20081124 <dammikaishan31@gmail.Com>
2024-09-21 13:17:11 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,8 +5,8 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "Ryc2STzD#K3fbUJYnz4YCE0Kd66CUBySE31k_lRCvm_yZxP6tdls",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/397000a07a1deb7fad9c2.jpg",
-MONGODB: process.env.MONGODB || "",
+MONGODB: process.env.MONGODB || "943340460",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 };
